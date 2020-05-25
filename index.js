a432a6ed787f8878dcdc0eedce37aab8f070da1c
Uzoamaka126 <vanessapeters126@gmail.com>
2020-05-25 22:32:46 +0100

    remove client url from env file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-// require('dotenv').config();
-const server = require('./app');
-const config = require('./Config')
+const server = require("./app");
+const config = require("./Config");
 
-// const port = process.env.PORT || 5000;
 const PORT = config.PORT;
 
 server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-})
+  console.log(`Server listening on port ${PORT}`);
+});
