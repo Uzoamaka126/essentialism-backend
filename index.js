a0efb4f3e4d5c9273228c6e42f5c9eca6016a7bb
Uzoamaka126 <vanessapeters126@gmail.com>
2020-05-25 22:02:01 +0100

    Set dotenv as a dev dependency

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
+// require('dotenv').config();
 const server = require('./app');
 const config = require('./Config')
 
