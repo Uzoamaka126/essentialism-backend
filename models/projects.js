69e05463a8ef55913ccecbd8af328b8b7b9987a6
Amaka Anyanwu <vanessapeters@126@gmail.com>
2021-02-20 01:16:18 +0100

    edit a single project

--- models/projects.js
+++ models/projects.js
@@ -1,4 +1,4 @@
-const db = require('../db-config')
+const db = require("../db-config");
 
 module.exports = {
   getUserProjects,
@@ -8,153 +8,156 @@ module.exports = {
   deleteUserProjects,
   findUsersByProjects,
   getUserProjectsByValue,
-  getAllProjects
-}
+  getAllProjects,
+};
 
 // @TODO: Get a project by the project id
-async function findUsersByProjects (id) {
+async function findUsersByProjects(id) {
   try {
-    const response = await db('projects as p')
-      .join('users as u', 'u.id', 'p.userId')
-      .join('values as v', 'v.id', 'p.valueId')
+    const response = await db("projects as p")
+      .join("users as u", "u.id", "p.userId")
+      .join("values as v", "v.id", "p.valueId")
       .select(
-        'p.id',
-        'p.userId',
-        'p.valueId',
-        'p.projectId',
-        'title',
-        'v.value_name',
-        'v.description',
-        'createdAt',
-        'updatedAt'
+        "p.id",
+        "p.userId",
+        "p.valueId",
+        "p.projectId",
+        "title",
+        "v.value_name",
+        "v.description",
+        "createdAt",
+        "updatedAt"
       )
-      .where({ 'p.id': id })
-      .first()
-    return response
+      .where({ "p.id": id })
+      .first();
+    return response;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 // @TODO: Create a new project
-async function addUserProjects (project) {
+async function addUserProjects(project) {
   try {
-    const ids = await db('projects as p')
-      .insert(project, 'id')
-      .join('users as u', 'u.id', 'p.userId')
-      .join('values as v', 'v.id', 'p.valueId')
-      .where({ 'u.id': project.userId, 'v.id': project.valueId })
-    const id = ids[0]
-    const response = await findUsersByProjects(id)
-    return response
+    const ids = await db("projects as p")
+      .insert(project, "id")
+      .join("users as u", "u.id", "p.userId")
+      .join("values as v", "v.id", "p.valueId")
+      .where({ "u.id": project.userId, "v.id": project.valueId });
+    const id = ids[0];
+    const response = await findUsersByProjects(id);
+    return response;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 // @TODO: Get a list of projects by user id and value id
-async function getUserProjectsByValue (userId, valueId) {
+async function getUserProjectsByValue(userId, valueId) {
   try {
-    const projects = await db('projects as p')
-      .join('users as u', 'u.id', 'p.user_id')
-      .join('values as v', 'v.id', 'p.value_id')
-      .select('p.id', 'u.id', 'project_name', 'v.value_name', 'v.description')
-      .where({ 'u.id': userId })
-      .andWhere({ 'v.id': valueId })
-    return projects
+    const projects = await db("projects as p")
+      .join("users as u", "u.id", "p.user_id")
+      .join("values as v", "v.id", "p.value_id")
+      .select("p.id", "u.id", "project_name", "v.value_name", "v.description")
+      .where({ "u.id": userId })
+      .andWhere({ "v.id": valueId });
+    return projects;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 // @TODO: Get all projects
-async function getAllProjects () {
+async function getAllProjects() {
   try {
-    const projects = await db('projects as p')
-      .join('users as u', 'u.id', 'p.userId')
-      .join('values as v', 'v.id', 'p.valueId')
+    const projects = await db("projects as p")
+      .join("users as u", "u.id", "p.userId")
+      .join("values as v", "v.id", "p.valueId")
       .select(
-        'p.id',
-        'p.userId',
-        'title',
-        'v.value_name',
-        'v.description',
-        'createdAt',
-        'updatedAt',
-        'u.id',
-        'u.userId'
-      )
-    return projects
+        "p.id",
+        "p.userId",
+        "title",
+        "v.value_name",
+        "v.description",
+        "createdAt",
+        "updatedAt",
+        "u.id",
+        "u.userId"
+      );
+    return projects;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 // @TODO: Get a list of project by user id
-async function getUserProjects (userId) {
+async function getUserProjects(userId) {
   try {
-    const projects = await db('projects as p')
-      .join('users as u', 'u.id', 'p.userId')
-      .join('values as v', 'v.id', 'p.valueId')
+    const projects = await db("projects as p")
+      .join("users as u", "u.id", "p.userId")
+      .join("values as v", "v.id", "p.valueId")
       .select(
-        'p.id',
-        'p.userId',
-        'p.valueId',
-        'title',
-        'p.projectId',
-        'v.value_name',
-        'v.description',
-        'createdAt',
-        'updatedAt'
+        "p.id",
+        "p.userId",
+        "p.valueId",
+        "title",
+        "p.projectId",
+        "v.value_name",
+        "v.description",
+        "createdAt",
+        "updatedAt"
       )
-      .where({ 'u.id': userId })
-    return projects
+      .where({ "u.id": userId });
+    return projects;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 // @TODO: Get a single project by user id and project id
-async function getUserSingleProject (userId, id) {
+async function getUserSingleProject(payload) {
   try {
-    const project = await db('projects as p')
-      .join('users as u', 'u.id', 'p.user_id')
-      .join('values as v', 'v.id', 'p.value_id')
-      .select('p.id',
-        'p.user_id',
-        'project_name',
-        'v.value_name',
-        'v.description',
-        'createdAt',
-        'updatedAt'
+    const project = await db("projects as p")
+      .join("users as u", "u.id", "p.userId")
+      .join("values as v", "v.id", "p.valueId")
+      .select(
+        "p.id",
+        "p.userId",
+        "p.valueId",
+        "title",
+        "p.projectId",
+        "v.value_name",
+        "v.description",
+        "createdAt",
+        "updatedAt"
       )
-      .where({
-        'p.id': id,
-        'p.user_id': userId
-      })
-      .first()
-    return project
+      .where({ "u.id": payload.userId, "p.id": payload.id })
+      .first();
+    return project;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 // @TODO: Edit a project by user id and project id
-async function editUserProject (project) {
+async function editUserProject(project) {
   try {
-    const response = await db('projects as p')
-      .join('users as u', 'u.id', 'p.userId')
-      .join('values as v', 'v.id', 'p.valueId')
-      .where({ id: project.id })
-      .update({ 'p.title': project.title, 'p.valueId': project.valueId })
-    return response
+    const ids = await db("projects as p")
+      .join("users as u", "u.id", "p.userId")
+      .join("values as v", "v.id", "p.valueId")
+      .update({
+        title: project.title,
+        valueId: project.valueId,
+      })
+      .where({ id: project.id });
+    return ids;
   } catch (err) {
-    console.log(err)
-    return err
+    console.log(err);
+    return err;
   }
 }
 
 // @TODO: Delete a project based on the project id
-function deleteUserProjects (id) {
-  return db('projects').where({ id: id }).del()
+function deleteUserProjects(id) {
+  return db("projects").where({ id: id }).del();
 }
--- routes/projects.js
+++ routes/projects.js
@@ -1,107 +1,107 @@
-const express = require('express')
-const router = express.Router()
-const service = require('../services/projects')
-const { validate } = require('../helpers/protectedMiddleware')
+const express = require("express");
+const router = express.Router();
+const service = require("../services/projects");
+const { validate } = require("../helpers/protectedMiddleware");
 
 // @TODO: Create a new project
-router.post('/create', validate, async (req, res, next) => {
+router.post("/create", validate, async (req, res, next) => {
   try {
-    const response = await service.createUserProject(req.body)
+    const response = await service.createUserProject(req.body);
     if (!response) {
-      return res.status(response.status).json(response)
+      return res.status(response.status).json(response);
     }
-    res.status(response.status).json(response)
+    res.status(response.status).json(response);
   } catch (err) {
-    console.log(err)
-    next(err)
+    console.log(err);
+    next(err);
   }
-})
+});
 
 // @TODO: Update a single project
-router.put('/update', validate, async (req, res, next) => {
+router.put("/update", validate, async (req, res, next) => {
+  const { userId, title, id, valueId } = req.body;
   try {
-    const response = await service.updateProject(req.body)
-
-    return res.status(response.status).json(response)
+    const response = await service.updateProject({ userId, title, id, valueId });
+    return res.status(response.status).json(response);
   } catch (err) {
-    console.log(err)
-    next(err)
+    console.log(err);
+    next(err);
   }
-})
+});
 
 // @TODO: Get a list of all projects
-router.get('/projects', validate, async (req, res, next) => {
+router.get("/projects", validate, async (req, res, next) => {
   try {
-    const projects = await service.fetchAllProjects()
-    res.status(projects.status).json(projects)
+    const projects = await service.fetchAllProjects();
+    res.status(projects.status).json(projects);
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 // @TODO: Get a list of projects by user id
-router.post('/fetchProjects', validate, async (req, res, next) => {
+router.post("/fetchProjects", validate, async (req, res, next) => {
   try {
-    const userId = req.body.userId
-    const result = await service.fetchAllUserProjects(userId)
-    res.status(result.status).json(result)
+    const userId = req.body.userId;
+    const result = await service.fetchAllUserProjects(userId);
+    res.status(result.status).json(result);
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 // @TODO: Get single project
-router.get('/:id', validate, async (req, res, next) => {
+router.get("/:id", validate, async (req, res, next) => {
   try {
-    const userId = req.user.subject
-    const id = req.params.id
-    const project = await service.fetchSingleProject(userId, id)
+    const userId = req.user.subject;
+    const id = req.params.id;
+    const project = await service.fetchSingleProject(userId, id);
 
-    res.status(project.status).json(project)
+    res.status(project.status).json(project);
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 // @TODO: Get list of projects for a particular value
-router.get('/get', validate, async (req, res) => {
+router.get("/get", validate, async (req, res) => {
   try {
-    const { value_id } = req.query
-    const { user_id } = req.user.subject
+    const { value_id } = req.query;
+    const { user_id } = req.user.subject;
 
     if (!value_id && !user_id) {
       return res.status(404).json({
-        message: 'No value id or user id was specified in the query string'
-      })
+        message: "No value id or user id was specified in the query string",
+      });
     }
 
-    const result = await service.fetchAllUserProjectsByValue(user_id, value_id)
+    const result = await service.fetchAllUserProjectsByValue(user_id, value_id);
     if (!result) {
-      return res.status(result.status).json(result.message)
+      return res.status(result.status).json(result.message);
     }
-    return res.status(result.status).json(result)
+    return res.status(result.status).json(result);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
 // @TODO: Delete a specified project by id
-router.delete('/delete/:id', validate, async (req, res, next) => {
-  const { id } = req.params
+router.delete("/delete/:id", validate, async (req, res, next) => {
+  const { id } = req.params;
   if (!id) {
-    return res.status(404).json({ message: 'Id not provided' })
+    return res.status(404).json({ message: "Id not provided" });
   }
 
   try {
-    const response = await service.removeUserProjects(id)
-    return res.status(response.status).json(response)
+    const response = await service.removeUserProjects(id);
+    return res.status(response.status).json(response);
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
